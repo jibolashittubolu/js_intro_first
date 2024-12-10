618e73865fbca3801c434ec442ad5c53d945b4fb
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2024-12-11 00:02:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById('name').innerHTML= `Name: ${name}`
 while(!name){
     name = window.prompt('Enter your name:')
     document.getElementById('name').innerHTML = `Name: ${name}`
-}
+} 
 
 var height = window.prompt('Enter your height:')
 document.getElementById('height').innerHTML = `Height: ${height}`
